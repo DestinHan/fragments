bf350530e689cc78137b5bcf9441f642e3106088
DestinHan <seung4530@naver.com>
2025-09-09 11:20:59 -0400

    unit test fixed

--- src/auth/strategies.js
+++ src/auth/strategies.js
@@ -1,37 +1,7 @@
-// src/auth/strategies.js
 const passport = require('passport');
 const { BasicStrategy } = require('passport-http');
 const { Strategy: BearerStrategy } = require('passport-http-bearer');
 const { CognitoJwtVerifier } = require('aws-jwt-verify');
-
-const userPoolId = process.env.AWS_COGNITO_POOL_ID;
-const clientId   = process.env.AWS_COGNITO_CLIENT_ID;
-
-// Bearer (Cognito)
-const verifier = CognitoJwtVerifier.create({
-  userPoolId,
-  tokenUse: 'id',
-  clientId,
-});
-
-verifier.hydrate?.().catch(() => { /* noop */ });
-
-passport.use(
-  'bearer',
-  new BearerStrategy(async (token, done) => {
-    try {
-      const payload = await verifier.verify(token);
-      const user = {
-        sub: payload.sub,
-        email: payload.email,
-        username: payload['cognito:username'],
-      };
-      return done(null, user);
-    } catch {
-      return done(null, false);
-    }
-  })
-);
 
 passport.use(
   'http',
@@ -40,5 +10,28 @@ passport.use(
     return done(null, email);
   })
 );
+
+const userPoolId = process.env.AWS_COGNITO_POOL_ID;
+const clientId = process.env.AWS_COGNITO_CLIENT_ID;
+
+if (userPoolId && clientId) {
+  const verifier = CognitoJwtVerifier.create({
+    userPoolId,
+    clientId,
+    tokenUse: 'id',
+  });
+
+  passport.use(
+    'bearer',
+    new BearerStrategy(async (token, done) => {
+      try {
+        const payload = await verifier.verify(token);
+        return done(null, { sub: payload.sub, email: payload.email });
+      } catch {
+        return done(null, false);
+      }
+    })
+  );
+}
 
 module.exports = {};
