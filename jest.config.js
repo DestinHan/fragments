2c8a21fb5fa5e1abf94ddc19fd8a90d1d070a5c8
DestinHan <seung4530@naver.com>
2025-09-17 15:03:37 -0400

    minor change

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,11 @@
-// jest.config.js
 module.exports = {
   testEnvironment: 'node',
   collectCoverage: true,
   collectCoverageFrom: [
     'src/**/*.js',
-    '!src/server.js',         
-    '!src/logger.js',        
-    '!src/auth/cognito.js',    
-    '!src/auth/basic-auth.js', 
+    '!src/server.js',
+    '!src/logger.js',
+    '!src/auth/cognito.js',
+    '!src/auth/basic-auth.js',
   ],
 };
